342ef9c67f2855212b3478cccbcbfa9ec53b846d
Andy Korth <andy.korth@gmail.com>
2024-01-26 23:12:19 -0600

    player and camera!

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Player player;
+    public Vector3 offset;
+
+    public void LateUpdate()
+    {
+        this.transform.position = player.transform.position + offset;
+        this.transform.rotation = Quaternion.LookRotation(player.transform.position - transform.position);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+    public float accel = 0.5f;
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 target = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(target.x, 0f, target.y) * speed, accel);
+
+    }
+}
