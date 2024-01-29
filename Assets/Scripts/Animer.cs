e1a1ee718ffc558d08181a3f614d4b26e3b5d9da
Trentish <trentongreyoak@gmail.com>
2024-01-28 18:15:01 -0600

    walk anim

--- Assets/Scripts/Animer.cs
+++ Assets/Scripts/Animer.cs
@@ -4,9 +4,10 @@ public class Animer : MonoBehaviour
 {
 	public Animator Animator;
 
-	public void SetWalking(bool isWalking)
+	public void SetWalking(bool isWalking, float speed)
 	{
 		Animator.SetBool("Walking", isWalking);
+		Animator.SetFloat("Speed", speed);
 	}
 	
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,32 @@
 using Foundational;
 using futz.ActGhost;
+using Lumberjack;
+using Swoonity.CSharp;
+using Swoonity.Unity;
 using UnityEngine;
 
 public class Player : Singleton<Player>
 {
 	public Animer Animer;
+	public Transform DirFlipper;
 	public float WalkThreshold = .1f;
+	public float CharacterScale = .2f;
+	public float AnimWalkSpeedMin = .1f;
+	public float AnimWalkSpeedMax = 3f;
+	public float AnimWalkVelocityMin = 0f;
+	public float AnimWalkVelocityMax = 5f;
 	public float speed = 3f;
-	public float accel = 0.5f;
 
 	public Rigidbody rb;
+
+	void LateUpdate()
+	{
+		var target = new Vector2(
+				Input.GetAxis("Horizontal"),
+				Input.GetAxis("Vertical"))
+			.Clamp();
+		rb.AddForce(new Vector3(target.x, target.y) * speed * Time.deltaTime);
+	}
 
 	void Update()
 	{
@@ -18,11 +35,19 @@ public class Player : Singleton<Player>
 
 		if (act.Phase.Current != GhostActivity.PhaseEnum.PLAYING_ROOM) return;
 
-		Vector2 target = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		rb.AddForce(new Vector3(target.x, target.y) * speed * Time.deltaTime);
 
 		var velocityMag = rb.velocity.magnitude;
-		Animer.SetWalking(velocityMag >= WalkThreshold);
+
+		var animSpeed = velocityMag.Map(
+			(AnimWalkVelocityMin, AnimWalkVelocityMax),
+			(AnimWalkSpeedMin, AnimWalkSpeedMax)
+		);
+		Animer.SetWalking(velocityMag >= WalkThreshold, animSpeed);
+		// $"{velocityMag}".LgOrange0();
+
+		DirFlipper.localScale = rb.velocity.x <= 0
+			? new Vector3(1, 1, 1) * CharacterScale
+			: new Vector3(-1, 1, 1) * CharacterScale;
 
 		// rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(target.x, 0f, target.y) * speed, accel);
 
